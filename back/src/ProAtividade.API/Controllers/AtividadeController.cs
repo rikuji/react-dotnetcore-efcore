e24b49819e9243b34e5ac65d54d0789fd2737537
Lucas Bezerra <lucas.bezerra@celcoin.com.br>
2021-11-08 12:37:33 -0300

    Seção 02 - ASP.NET 5 - Introdução

---
+++ back/src/ProAtividade.API/Controllers/AtividadeController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProAtividade.API.Models;
+
+namespace ProAtividade.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AtividadeController : ControllerBase
+    {
+        public IEnumerable<Atividade> Atividades = new List<Atividade>() {
+            new Atividade(1),
+            new Atividade(2),
+            new Atividade(3)
+        };
+
+        [HttpGet]
+        public IEnumerable<Atividade> Get()
+        {
+            return Atividades;
+        }
+
+        [HttpGet("{id}")]
+        public Atividade Get(int id)
+        {
+            return Atividades.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public IEnumerable<Atividade> Post([FromBody] Atividade atividade)
+        {
+            return Atividades.Append<Atividade>(atividade);
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return "Put";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return "Delete";
+        }
+    }
+}
---
+++ back/src/ProAtividade.API/Models/Atividade.cs
@@ -0,0 +1,16 @@
+﻿namespace ProAtividade.API.Models
+{
+    public class Atividade
+    {
+        public Atividade() { }
+
+        public Atividade(int id)
+        {
+            Id = id;
+        }
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descrição { get; set; }
+        public string Prioridade { get; set; }
+    }
+}
